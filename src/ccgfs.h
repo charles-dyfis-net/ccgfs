ce617e9cfbf463f1757909cf118727afad1af8a1
Jan Engelhardt <jengelh@computergmbh.de>
2007-08-19 16:52:37 +0000

    Implement proper byte-swapping

--- src/ccgfs.h
+++ src/ccgfs.h
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 #include <stdint.h>
+#include "config.h"
 
 /*
  * min()/max() macros that also do
@@ -22,10 +23,37 @@
 	(_x > _y) ? _x : _y;	\
 })
 
-#define le32_to_cpu(x) (x)
-#define le64_to_cpu(x) (x)
-#define cpu_to_le32(x) (x)
-#define cpu_to_le64(x) (x)
+#ifdef PKTSWAP
+static inline uint16_t swap16(uint16_t x)
+{
+	return ((x & 0xFF) << 8) | ((x & 0xFF00) >> 8);
+}
+static inline uint32_t swap32(uint32_t x)
+{
+	return ((x & 0x000000FF) << 24) |
+	       ((x & 0x0000FF00) << 8) |
+	       ((x & 0x00FF0000) >> 8) |
+	       ((x & 0xFF000000) >> 24);
+}
+static inline uint64_t swap64(uint64_t x)
+{
+	return ((uint64_t)swap32(x & 0xFFFFFFFF) << 32) |
+	       (swap32(x >> 32) & 0xFFFFFFFF);
+}
+#	define le16_to_cpu(x) swap16(x)
+#	define le32_to_cpu(x) swap32(x)
+#	define le64_to_cpu(x) swap64(x)
+#	define cpu_to_le16(x) swap16(x)
+#	define cpu_to_le32(x) swap32(x)
+#	define cpu_to_le64(x) swap64(x)
+#else
+#	define le16_to_cpu(x) (x)
+#	define le32_to_cpu(x) (x)
+#	define le64_to_cpu(x) (x)
+#	define cpu_to_le16(x) (x)
+#	define cpu_to_le32(x) (x)
+#	define cpu_to_le64(x) (x)
+#endif
 
 #define reinterpret_cast(type, x) ((type)(x))
 #define static_cast(type, x)      ((type)(x))
